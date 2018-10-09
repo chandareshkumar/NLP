509372a2f92e268cb7e1d2e5f03b2d107ac69f35
Chandareshkumar <chandareshkumar@gmail.com>
2018-10-09 08:21:21 +0530

    Added comments

--- NLTK_puzzle.py
+++ NLTK_puzzle.py
@@ -1,44 +1,17 @@
 
-# coding: utf-8
-
-# In[1]:
-
 
 import nltk
 
 
-# In[2]:
-
+# forming the possible words using given alphabet
 
 puzzle= nltk.FreqDist('egivrvonl')
 
-
-# In[3]:
-
-
 mandate='r'
 
-
-# In[4]:
-
-
 word_list=nltk.corpus.words.words()
-
-
-# In[5]:
-
-
-word_list
-
-
-# In[8]:
-
 
 puz=[w for w in word_list if len(w)>=6 and mandate in w and nltk.FreqDist(w)<puzzle]
 
 
-# In[9]:
-
-
-puz
-
+print(puz)
